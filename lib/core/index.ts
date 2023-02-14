8ffd3e47fc3298fbf67eddc5b4cd7f9470011712
Washington Junior <washingtonj@outlook.com.br>
2023-02-14 12:51:24 -0300

    feat: change functions return

--- lib/core/index.ts
+++ lib/core/index.ts
@@ -22,34 +22,32 @@ function beject<T = any>(data: T[] = [], hooks?: Hooks<T[]>) {
     create(params: Clear<Fn.CreateProps<T>>) {
       const out = fn.create({ input: data, ...params })
       hooks?.afterRun?.(out)
-      return beject(out, hooks)
+      return out
     },
 
     update(params: Clear<Fn.UpdateProps<T>>) {
       const out = fn.update({ input: data, ...params })
       hooks?.afterRun?.(out)
-      return beject(out, hooks)
+      return out
     },
 
     remove(params: Clear<Fn.RemoveProps<T>>) {
       const out = fn.remove({ input: data, ...params })
       hooks?.afterRun?.(out)
-      return beject(out, hooks)
+      return out
     },
 
     updateByKey(params: Clear<Fn.UpdateByKeyProps<T>>) {
       const out = fn.updateByKey({ input: data, ...params })
       hooks?.afterRun?.(out)
-      return beject(out, hooks)
+      return out
     },
 
-    createKeepRemove(params: Clear<Fn.CreateKeepRemoveProps<T, keyof T>>) {
+    createKeepRemove<_, K extends keyof T>(params: Clear<Fn.CreateKeepRemoveProps<T, K>>) {
       const out = fn.createKeepRemove({ input: data, ...params })
       hooks?.afterRun?.(out)
-      return beject(out, hooks)
-    },
-
-    data
+      return out
+    }
   }
 }
 
